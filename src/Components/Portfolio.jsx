ceb90310c5b0344b1309f58260556bf890cad570
sreekalakannan <Sreekalakannan33@gmail.com>
2024-09-09 11:15:05 +0530

    first commit

---
+++ src/Components/Portfolio.jsx
@@ -0,0 +1,95 @@
+import React from "react";
+
+
+
+function Portfolio(){
+    return(
+      
+        <div className="container" style={{width:'100wh',padding:'50px'}}>
+           <div>
+              <div style={{display:'flex',flex:'column',marginLeft:'870px'}}>
+                
+                <h4  style={{marginRight:'10px'}}>SreeKala M</h4>
+                <p className="bg-red-500 h-2"></p>  
+              
+                
+               <img style={{width:'150px',height:'200px'}} src={require('../Assest/WhatsApp Image 2024-08-20 at 9.37.27 PM.jpeg')} alt="" />
+              
+              </div>
+              <a style={{marginLeft:'30px'}} href="mailto:Sreekalakannan33@gmail.com">Sreekalakannan33@gmail.com</a>
+              
+                
+               
+                
+            
+               <div>
+                <p  style={{backgroundColor:'blue',width:'1200px'}}>Career objective</p>
+          
+                <p style={{width:'1000px'}}>A motivated individual with in-depth knowledge of languages and development tools, seeking a
+position in a growth-oriented company where I can use my skills to the advantage of the
+company while having the scope to develop my skills.</p>
+               </div>
+               <div>
+               <p  style={{backgroundColor:'blue',width:'1200px'}}>ACADEMIC PROFICIENCY</p>
+              <p>Under Graduation Degree (2021)</p>
+               B.COM (CA) with CGPA: 6.83 aggregate from Kathir College of Arts and Science, Coimbatore, Affiliated to Bharathiyar University, Chennai
+               <p>HSLC (2018)</p>
+               <p>H.S.L.C with 90.5% aggregate from Sri Ramakrishna Vidyalaya Matric Higher Secondary School, Tiruppur</p>
+               <p>SSLC (2016)</p>
+              <p> S.S.L.C with 81.2% aggregate from Kongu Kalvi Nilayam, Tiruppur.</p>
+               </div>
+               <div>
+               <p  style={{backgroundColor:'blue',width:'1200px'}}>TECHNICAL SKILLS</p>
+               <p> HTML and CSS</p>
+               <p>  Fundamentals of JavaScript </p>
+               <p>  Javascript es6</p>
+               <p>  DOM and Event Handling Facility</p>
+               <p>  Node, npm</p>
+               <p>  Redux</p>
+               <p>  Fetch data from Res</p>
+               </div>
+               <div>
+               <p  style={{backgroundColor:'blue',width:'1200px'}}>SOFT SKILLS</p>
+               <p> Excellent Communication</p>
+               <p> Problem-Solving Approach</p>
+               <p>  Ability to Work in a Team</p>
+                         
+               </div>
+               <div>
+               <p  style={{backgroundColor:'blue',width:'1200px'}}>PROJECTS</p>
+<h3> PROJECT DETAILS:Create TVFLIX Website DESIGN</h3>
+<h2>PROJECT NAME:TVFLIX</h2>
+ <h2>ENVIRONMENT:HTML,CSS,JAVASCRIPT,REACTJS</h2>              </div>
+<div>
+ <p  style={{backgroundColor:'blue',width:'1200px'}}>CERIFICATION</p>
+
+<p> Certification in REACTJS at BESANT Technology, Chennai </p>
+ </div>
+ <div>
+ <p  style={{backgroundColor:'blue',width:'1200px'}}>PERSONAL PROFILE</p>
+ <p>PERSONAL PROFILE</p>
+ <p>Father`s name : Mr.A.Murugesan</p>
+<p>Mother`s name : Mrs.M.Manjula</p>
+<p>Date of Birth : 07.10.2000</p>
+<p>Age : 24</p>
+<p>Gender : Female</p>
+<p>Nationality : Indian</p>
+<p>Religion : Hindu</p>
+<p>Language. :Tamil,Malayalam and English (to speak and write)</p>
+<p>Address :81,venkataachalapathi Nagar,Devampalayam, Avinashi- 641654</p>
+
+<div>
+<p  style={{backgroundColor:'blue',width:'1200px'}}>DECALARATION</p>
+<p>  I hereby declare that all the information furnished above is true to the best of my
+knowledge and belief</p>
+</div>
+
+ </div>
+
+        </div>
+        </div>
+      
+    )
+}
+
+export default Portfolio
